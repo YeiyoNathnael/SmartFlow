fb62164241b7ce908bab7603165568c7c09bc636
YeiyoNathnael <138720671+YeiyoNathnael@users.noreply.github.com>
2026-04-09 17:36:22 +0400

    Init: Creating the starter maven format for the java system

---
+++ SmartFlow/src/main/java/nathnael/abatye/adu/ac/ae/App.java
@@ -0,0 +1,13 @@
+package nathnael.abatye.adu.ac.ae;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
